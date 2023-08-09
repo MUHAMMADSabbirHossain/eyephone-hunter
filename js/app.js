9f490cec1a3967638269d3ddd0d0a913df536cfa
muhammad bin ahmad <dustobalok000@gmail.com>
2023-08-09 22:42:22 +0600

    id grabed

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const inputField = document.getElementById('input-field');
+console.log(inputField);
+const searchBtn = document.getElementById('search-btn');
+console.log(searchBtn);
+const errorSection = document.getElementById('error-section');
+console.log(errorSection);
+const mainSection = document.getElementById('main-section');
+console.log(mainSection);
+
+
